64b8f1276bb722424d1278a13d9c34ffb1613857
hlahtoo <hlahtoo1998@gmail.com>
2025-03-31 16:26:17 -0700

    Set up database and finished implementing side bars and initial routing

---
+++ src/app/(protected)/create/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+type FormInput = {
+  repoUrl: string;
+  projectName: string;
+  githubToken?: string;
+};
+
+const CreatePage = () => {
+  const { register, handleSubmit, reset } = useForm<FormInput>();
+
+  function onSubmit(data: FormInput) {
+    window.alert(JSON.stringify(data, null, 2));
+    return true;
+  }
+  return (
+    <div className="flex h-full items-center justify-center gap-12">
+      <img src="/undraw_github.svg" className="h-56 w-56" />
+      <div>
+        <div>
+          <h1 className="text-2xl font-semibold">
+            Link your Github Repository
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Enter the URL of your Repository to linkk it to CommitGenie
+          </p>
+        </div>
+        <div className="h-4"></div>
+        <div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              {...register("projectName", { required: true })}
+              placeholder="Project Name"
+              required
+            />
+            <div className="h-2"></div>
+            <Input
+              {...register("repoUrl", { required: true })}
+              placeholder="Github URL"
+              type="url"
+              required
+            />
+            <div className="h-2"></div>
+            <Input
+              {...register("githubToken")}
+              placeholder="Github Token (Optional)"
+            />
+            <div className="h-4"></div>
+            <Button type="submit">Create Project</Button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePage;
---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,31 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { UserButton } from "@clerk/nextjs";
+import { ProxyPromise } from "@trpc/server/unstable-core-do-not-import";
+import React, { Children } from "react";
+import { AppSidebar } from "./dashboard/app-sidebar";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const SidebarLayout = ({ children }: Props) => {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="m-2 w-full">
+        <div className="flex items-center gap-2 rounded-md border border-sidebar-border bg-sidebar p-2 px-4 shadow">
+          {/* <SearchBar /> */}
+          <div className="ml-auto"></div>
+          <UserButton />
+        </div>
+        <div className="h-4"></div>
+        {/* main content */}
+        <div className="h-[calc(100vh-6rem)] overflow-y-scroll rounded-md border border-sidebar-border bg-sidebar p-4 shadow">
+          {children}
+        </div>
+      </main>
+    </SidebarProvider>
+  );
+};
+
+export default SidebarLayout;
